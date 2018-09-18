999a7068dd4181cfbc21bcce98cd089e6d6ec729
mbyk <marubayashi@gmail.com>
2018-09-18 23:30:00 +0900

    aceエディタの組み込み

--- components/EnvEditor.vue
+++ components/EnvEditor.vue
@@ -1,18 +1,34 @@
 <template>
   <div class="envEditorContainer">
+		<pre id="editor">
+{
+"key": "xxxxxxxxxx",
+"mode": "production"
+}
+		</pre>
 	</div>
 </template>
 
 <script>
-	
-export default {
 
+export default {
+  head: {
+    script: [
+      { src: '/src/ace.js' }
+    ]
+	},
 	data() {
 		return {
 		}
 	},
 
 	methods: {
+	},
+
+	mounted() {
+		var editor = ace.edit("editor");
+		editor.setTheme("ace/theme/tommorrow");
+		editor.session.setMode("ace/mode/json");
 	}
 }
 
@@ -21,6 +37,12 @@ export default {
 <style scoped>
 
 .envEditorContainer {
+	width: 100%;
+	height: 100%;
+}
+
+#editor {
+	margin: 0;
 	width: 100%;
 	height: 100%;
 }
